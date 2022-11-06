67c75114aea7bc3602905307957d72100268cddb
PrajaktaShewale <shewalep94@gmail.com>
2022-11-06 20:22:52 +0530

    [Prajakta]Add:VendingMachineProblem

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -13,7 +13,8 @@ namespace LogicalPrograms
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Stop Watch" + "\n" + "7. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Stop Watch" + "\n" +
+                    "7. Vending Machine" + "\n"+ "8. Exit");
 
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -40,6 +41,10 @@ namespace LogicalPrograms
                     case 6:
                         StopWatch watch = new StopWatch();
                         watch.Start();
+                        break;
+                    case 7:
+                        VendingMachine Ven = new VendingMachine();
+                        Ven.Notes(2125);
                         break;
                     default:
                         flag = false;
