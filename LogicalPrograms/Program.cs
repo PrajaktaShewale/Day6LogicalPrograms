aa7ce85b03e5c41d7bc48957d43d5ae21f03ed4f
PrajaktaShewale <shewalep94@gmail.com>
2022-11-08 16:09:07 +0530

    [Prajakta]Add:TempConversionProblem

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -14,7 +14,7 @@ namespace LogicalPrograms
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
                 Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Stop Watch" + "\n" +
-                    "7. Vending Machine" + "\n" + "8. DayOfWeek" + "\n"+ "9. Exit");
+                    "7. Vending Machine" + "\n" + "8. DayOfWeek" + "\n" + "9. Temprature Conversion" + "\n" + "10. Exit");
 
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -48,6 +48,22 @@ namespace LogicalPrograms
                         break;
                     case 8:
                         DayOfWeek.DayOf();
+                        break;
+                    case 9:
+                        TemperatureConversion conversion = new TemperatureConversion();
+                        Console.WriteLine("1. CelciusToFarhnite \n 2. FarnhiteToCelcius ");
+                        int input = Convert.ToInt32(Console.ReadLine());
+                        if (input == 1)
+                        {
+                            float f = conversion.CelsiusConvert(100);
+                            Console.WriteLine("Celcius To Farnhite is " + f);
+                        }
+                        else
+                        {
+                            float c = conversion.FahrenheitConvert(60);
+                            Console.WriteLine("Franhite to Celcius is : " + c);
+                        }
+
                         break;
                     default:
                         flag = false;
---
+++ LogicalPrograms/TemperatureConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class TemperatureConversion
+    {
+        public float CelsiusConvert(float number)
+        {
+            float fahrenheit;
+            fahrenheit = (number * 9/5) + 32;
+            return fahrenheit;
+        }
+        public float FahrenheitConvert(float number)
+        {
+            float celsius;
+            celsius = (number - 32) * 5/9;
+            return celsius;
+        }
+    }
+}
